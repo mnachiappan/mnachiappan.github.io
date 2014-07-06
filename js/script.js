cbed7f77264a9b8063dcf2bacf30b3a102a5f871
mnachiappan <mnachiappan1995@gmail.com>
2014-07-06 00:40:29 -0400

    Created external links menu, with animation.

--- js/script.js
+++ js/script.js
@@ -65,7 +65,7 @@ $(document).ready(function(){
                 if (target.length) {
                     $("html, body").animate({
                         scrollTop: target.offset().top
-                    }, 1000);
+                    }, 1500);
                     return false;
                 }
             }
