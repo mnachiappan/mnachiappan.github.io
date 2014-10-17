81cd6ae0b490490beceba866ce6251ce45be6a14
Meyyappan Nachiappan <mnachiappan1995@gmail.com>
2014-10-16 19:16:09 -0700

    updated reddit site

--- PebbleReddit/js/controllers.js
+++ PebbleReddit/js/controllers.js
@@ -1,23 +1,66 @@
 'use strict';
 
 redditApp.factory('Reddit', ['$http', function($http){
+	$http.defaults.useXDomain = false;
 	return {
 		getRecentHeadlines: function(query, callback) {
-			$http.get('http://www.reddit.com/r/' + query + '/new.json')
-				.success(function (data){
+			console.log("getting recentHeadlines");
+			/*
+			$.ajax('http://www.reddit.com/r/' + query + '/new.json',
+			{
+				async: true,
+				crossDomain: true,
+				type: 'GET'
+				
+			});
+*/
+			/*
+			$http({method: 'JSON',
+					url: 'http://www.reddit.com/r/' + query + '/new.json'})
+			.success(function (data, status){
+					//console.log("in success");
+					//console.log(data);
 					callback(null, data);
 				})
-				.error(function (e){
-					callback(e);
+				.error(function (data, status){
+					//console.log("in error");
+					//console.log(e);
+					callback(status);
+				});
+			*/
+			$http.get('http://www.reddit.com/r/' + query + '/new.json')
+				.success(function (data, status){
+					//console.log("in success");
+					//console.log(data);
+					callback(null, data);
+				})
+				.error(function (data, status){
+					//console.log("in error");
+					//console.log(e);
+					callback(status);
 				});
 		},
 		getTopHeadlines: function (query, callback) {
-			$http.get('http://www.reddit.com/r/' + query + '/top.json')
-				.success(function (data){
+			/*
+			$http({method: 'JSON',
+					url: 'http://www.reddit.com/r/' + query + '/new.json'})
+			.success(function (data, status){
+					//console.log("in success");
+					//console.log(data);
 					callback(null, data);
 				})
-				.error (function (e){
-					callback(e);
+				.error(function (data, status){
+					//console.log("in error");
+					//console.log(e);
+					callback(status);
+				});
+			*/
+			$http.get('http://www.reddit.com/r/' + query + '/top.json')
+				.success(function (data, status){
+					callback(null, data);
+				})
+				.error (function (data, status){
+					callback(status);
 				});
 		}
 	};
@@ -29,21 +72,27 @@ redditApp.controller('redditController', ['$scope', 'Reddit',
   function($scope, Reddit){
   	var Reddit = Reddit;
   	$scope.query;
-
+  	$scope.errorMessage;
   	$scope.executeSearch = function(){
-  		console.log("executing search, query: " + $scope.query);
+  		$scope.errorMessage = null;
+  		//console.log("executing search, query: " + $scope.query);
   		Reddit.getRecentHeadlines($scope.query, function (error, data){
-  			if (!error){
+  			if (!error && data){
   				$scope.recentHeadlines = data.data.children;
   				Reddit.getTopHeadlines($scope.query, function(error, data){
-  					if (!error){
+  					if (!error && data){
   						$scope.topHeadlines = data.data.children;
+  						console.log($scope.topHeadlines);
+  						$scope.errorMessage = null;
   					} else{
   						console.log("error finding this query: " + $scope.query);
+  						$scope.errorMessage = "whoops no such subreddit";
+  						console.log($scope.errorMessage);
   					}
   				});
   			} else{
   				console.log("error finding this query: " + $scope.query);
+  				$scope.errorMessage = "whoops, no such subreddit";
   			}
   		})
   	}
@@ -52,3 +101,4 @@ redditApp.controller('redditController', ['$scope', 'Reddit',
     
     $scope.topHeadlines; 
   }]);
+
